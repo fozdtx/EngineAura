9af0f2a9a1bbf1e3e39cf49948e13cbafad74464
fozdtx <135956218+fozdtx@users.noreply.github.com>
2024-02-28 22:11:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+console.log('Hello World!');
+
+document.getElementById('demos').addEventListener('click', function(){
+  
+  
+  window.open('demos.html');
+  
+  
+})
+
+
+document.getElementById('addons').addEventListener('click', function(){
+  
+  
+  window.open('demos.html');
+  
+  
+})
+
+
+
+document.getElementById('plugins').addEventListener('click', function(){
+  
+  
+  window.open('demos.html');
+  
+  
+})
